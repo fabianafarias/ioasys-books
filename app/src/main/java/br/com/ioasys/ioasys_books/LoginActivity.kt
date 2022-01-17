14f58746189b2811cfb502b7344b918a1b0555d8
Fabiana Farias <farias.fabianam@gmail.com>
2022-01-17 20:47:29 -0300

    activity login: adicionando background e logo

---
+++ app/src/main/java/br/com/ioasys/ioasys_books/LoginActivity.kt
@@ -0,0 +1,11 @@
+package br.com.ioasys.ioasys_books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
