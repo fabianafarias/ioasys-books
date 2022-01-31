eaa6032ffb071cf5e2c801b826bc4c385cd1d9ec
Fabiana Farias <farias.fabianam@gmail.com>
2022-01-31 13:15:20 -0300

    Concluindo as mensagens de erro da LoginFragment

--- app/src/main/java/br/com/ioasys/ioasys_books/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/br/com/ioasys/ioasys_books/presentation/ui/fragments/LoginFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import br.com.ioasys.ioasys_books.databinding.FragmentLoginBinding
+import br.com.ioasys.ioasys_books.presentation.viewmodel.LoginViewModel
+import br.com.ioasys.ioasys_books.util.ViewState
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding: FragmentLoginBinding get() = _binding!!
+
+    private val viewModel : LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +28,35 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.enterButton.setOnClickListener {
-            findNavController().navigate(
-                LoginFragmentDirections.actionLoginFragmentToBookListFragment(5)
+            setListener()
+            addObserver()
+        }
+    }
+
+    private fun setListener() {
+        binding.run {
+            viewModel.login(
+               textFieldEditEmail.text.toString(),
+                textFieldEditPassword.text.toString()
             )
+        }
+
+    }
+
+
+    fun addObserver(){
+        viewModel.loggedUsedViewState.observe(viewLifecycleOwner) { state ->
+
+            when(state){
+                is ViewState.Success -> {
+                    findNavController().navigate(
+                        LoginFragmentDirections.actionLoginFragmentToBookListFragment(15)
+                    )
+                }
+                is ViewState.Error -> {
+                    binding.txtError.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
