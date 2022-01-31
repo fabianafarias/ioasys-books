386886ea728186ddcedd8e582188102512940461
Fabiana Farias <farias.fabianam@gmail.com>
2022-01-31 12:24:24 -0300

    Implementando o LoginViewModel

--- app/src/main/java/br/com/ioasys/ioasys_books/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/ioasys/ioasys_books/presentation/viewmodel/LoginViewModel.kt
@@ -1,8 +1,25 @@
 package br.com.ioasys.ioasys_books.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.ioasys.ioasys_books.util.ViewState
 
 class LoginViewModel : ViewModel() {
+
+    private val _loggedUsedViewState = MutableLiveData<ViewState<Boolean>>()
+    val loggedUsedViewState = _loggedUsedViewState as LiveData<ViewState<Boolean>>
+
+
+    fun login(email: String, password: String) {
+
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            _loggedUsedViewState.value = ViewState.Success(true)
+        }else{
+            _loggedUsedViewState.value = ViewState.Error(Throwable())
+        }
+
+    }
 
 
 }
