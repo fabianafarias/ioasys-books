59556532067724a1e5eef0ac4cff498527ee070f
Fabiana Farias <farias.fabianam@gmail.com>
2022-01-31 12:08:28 -0300

    Criando Util e ViewState

---
+++ app/src/main/java/br/com/ioasys/ioasys_books/util/ViewState.kt
@@ -0,0 +1,14 @@
+package br.com.ioasys.ioasys_books.util
+
+sealed class ViewState<out T> {
+
+    object Loading: ViewState<Nothing>()
+
+    data class Success<T>(
+        val data: T
+    ) : ViewState<T>()
+
+    data class Error(
+        val throwable: Throwable
+        ): ViewState<Nothing>()
+}
