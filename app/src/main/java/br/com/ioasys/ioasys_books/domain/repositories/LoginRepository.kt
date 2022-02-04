a215767b2552b958ad47ec993201ac316df1bd57
Fabiana Farias <farias.fabianam@gmail.com>
2022-02-04 20:33:00 -0300

    Criando repositório de login - implementação

--- app/src/main/java/br/com/ioasys/ioasys_books/domain/repositories/LoginRepository.kt
+++ app/src/main/java/br/com/ioasys/ioasys_books/domain/repositories/LoginRepository.kt
@@ -1,6 +1,9 @@
 package br.com.ioasys.ioasys_books.domain.repositories
 
+import br.com.ioasys.ioasys_books.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
 interface LoginRepository {
 
-    fun login(email: String, password: String)
+    fun login(email: String, password: String) : Flow<User>
 }
