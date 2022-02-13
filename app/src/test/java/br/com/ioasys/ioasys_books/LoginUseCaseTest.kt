21f5a8578dcbeecc8b17188dfa90d088fca9bb59
Fabiana Farias <farias.fabianam@gmail.com>
2022-02-13 14:06:11 -0300

    Implementando Testes com mockito

---
+++ app/src/test/java/br/com/ioasys/ioasys_books/LoginUseCaseTest.kt
@@ -0,0 +1,77 @@
+package br.com.ioasys.ioasys_books
+
+import br.com.ioasys.ioasys_books.domain.model.User
+import br.com.ioasys.ioasys_books.domain.model.exception.InvalidEmailException
+import br.com.ioasys.ioasys_books.domain.repositories.LoginRepository
+import br.com.ioasys.ioasys_books.domain.usecase.LoginUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+
+class LoginUseCaseTest {
+
+    @Mock
+    private lateinit var repository: LoginRepository
+    private lateinit var subject: LoginUseCase
+
+    private val userFake = User(
+        name = "Camp",
+        birthdate = "12/12/2021",
+        gender = "A",
+        accessToken = "Token",
+        id = "id"
+    )
+
+    @Before
+    fun before() {
+        MockitoAnnotations.openMocks(this)
+        subject = LoginUseCase(
+            loginRepository = repository,
+        scope = CoroutineScope(Dispatchers.IO)
+        )
+    }
+
+    @Test
+    fun `WHEN SUCCESS MUST RETURN USER`() = runBlocking {
+        stubOnSuccess()
+        subject.run(
+            params = LoginUseCase.Params(
+                email = "email",
+                password = "ffssfsgt"
+            )
+        ).collect {
+            assert(it == userFake)
+        }
+    }
+
+    @Test(expected = InvalidEmailException::class)
+    fun `WHEN EMPTY EMAIL MUST RETURN INVALIDEMAILEXCEPTION`() {
+        subject.run(
+            params = LoginUseCase.Params(
+                email = "",
+                password = "bshjsa"
+            )
+        )
+    }
+
+    private fun stubOnSuccess() {
+        whenever(
+            repository.login(
+                email = any(),
+                password = any()
+            )
+        ).thenAnswer {
+            flowOf(userFake)
+        }
+    }
+
+
+}
