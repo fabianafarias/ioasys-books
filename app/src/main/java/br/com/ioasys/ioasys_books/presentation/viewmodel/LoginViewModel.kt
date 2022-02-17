856547e57a002b2e1bb68ec7f89df0d03473829f
Fabiana Farias <farias.fabianam@gmail.com>
2022-02-17 15:15:23 -0300

    Removendo o looping infinito do login

--- app/src/main/java/br/com/ioasys/ioasys_books/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/ioasys/ioasys_books/presentation/viewmodel/LoginViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.ioasys.ioasys_books.domain.usecase.LoginUseCase
-import br.com.ioasys.ioasys_books.util.*
+import br.com.ioasys.ioasys_books.util.ViewState
+import br.com.ioasys.ioasys_books.util.postError
+import br.com.ioasys.ioasys_books.util.postNeutral
+import br.com.ioasys.ioasys_books.util.postSuccess
 
 class LoginViewModel(
     private val loginUseCase: LoginUseCase
@@ -14,7 +17,6 @@ class LoginViewModel(
     val loggedUserViewState = _loggedUserViewState as LiveData<ViewState<String>>
 
     fun login(email: String, password: String) {
-        _loggedUserViewState.postLoading()
         loginUseCase(
             params = LoginUseCase.Params(
                 email = email,
